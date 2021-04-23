efa051c58fafcbe48417a43d0019cf0e27895710
kjeon0901 <kjeon0901@naver.com>
2021-04-23 18:35:46 +0900

    file move file rename add machine_learning - 0426_1, 0426_2

--- python_code/machine_learning/0426_2.py
+++ python_code/machine_learning/0426_2.py
@@ -4,21 +4,21 @@ from sklearn.model_selection import train_test_split
 
 import pandas as pd
 
-'''
+"""
 꽃의 길이와 폭, 꽃받침의 길이와 폭 데이터를 가지고 지도학습을 한다. 이것으로 총 3개의 붓꽃 품종 중에서 어떤 종인지 맞춰본다. 
-'''
+"""
 # 붓꽃 데이터 세트를 로딩합니다. iris는 객체가 됨
 iris = load_iris()
 
-# iris.data는 Iris 데이터 세트에서 피처(feature)만으로 된 데이터를 numpy로 가지고 있습니다. 
+# iris.data는 Iris 데이터 세트에서 피처(feature)만으로 된 데이터를 numpy로 가지고 있습니다.
 iris_data = iris.data
 
-# iris.target은 붓꽃 데이터 세트에서 레이블(결정 값) 데이터를 numpy로 가지고 있습니다. 
+# iris.target은 붓꽃 데이터 세트에서 레이블(결정 값) 데이터를 numpy로 가지고 있습니다.
 iris_label = iris.target
-print('iris target값:', iris_label)
-print('iris target명:', iris.target_names)
+print("iris target값:", iris_label)
+print("iris target명:", iris.target_names)
 
-# 붓꽃 데이터 세트를 자세히 보기 위해 DataFrame으로 변환합니다. 
+# 붓꽃 데이터 세트를 자세히 보기 위해 DataFrame으로 변환합니다.
 iris_df = pd.DataFrame(data=iris_data, columns=iris.feature_names)
-iris_df['label'] = iris.target
+iris_df["label"] = iris.target
 iris_df.head(3)
