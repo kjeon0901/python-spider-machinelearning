048bff87443be45b2eb38ccb53b816eb50bdc67a
kjeon0901 <kjeon0901@naver.com>
2021-05-25 12:55:55 +0900

    add some additional explanation on Perceptron code

--- python_code/practice.py
+++ python_code/practice.py
@@ -44,29 +44,34 @@ class Perceptron(object):
 
         """
         rgen = np.random.RandomState(self.random_state)
-        self.w_ = rgen.normal(loc=0.0, scale=0.01, size=1 + X.shape[1])
-        # normal(loc=평균, scale=표준편차, size=몇개의 데이터를 뱉어줄까?) : 평균이 loc이고 표준편차가 scale인 정규분포(평균:0 분산:1) 데이터를 size만큼 뱉어 달라!
-        print(self.w_) # 3개의 랜덤값 들어가 있다. '''[ 0.01624345 -0.00611756 -0.00528172]'''
+        self.w_ = rgen.normal(loc=0.0, scale=0.01, size=1 + X.shape[1]) # normal(loc=평균, scale=표준편차, size=몇개의 데이터를 뱉어줄까?) : 평균이 loc이고 표준편차가 scale인 정규분포(평균:0 분산:1) 데이터를 size만큼 뱉어 달라!
+        print(self.w_) # 3개의 랜덤값으로 가중치 3개 초기화 '''[ 0.01624345 -0.00611756 -0.00528172]'''
+        
         self.errors_ = []
 
         for _ in range(self.n_iter): # 10바퀴만 돈다. 
             errors = 0
             for xi, target in zip(X, y): # xi = X row 한줄씩, target = y 하나씩  (xi, target은 늘 같은 row)
-                update = self.eta * (target - self.predict(xi))
-                self.w_[1:] += update * xi
-                self.w_[0] += update
-                errors += int(update != 0.0)
+                # 가중치 변화량 Δwj = η( y(i) - y^(i) ) * xj(i)
+                update = self.eta * (target - self.predict(xi)) # η( y(i) - y^(i) ) 까지만 구함. xi: X row 하나씩 들어감(vector).
+                                                                # target, self.predict(xi), self.eta 모두 scalar => update도 scalar값. 
+                self.w_[1:] += update * xi # update는 scalar, xi는 vector. xi 요소 하나하나에 update 곱해줌. (동시에 모든 가중치 업데이트. wj = wj + Δwj)
+                self.w_[0] += update # w0은 x값과 곱해주지 않고 x와는 무관하므로 xi 곱해주면 안됨!
+                errors += int(update != 0.0) # y != y^인 경우 update != 0.0. 두 번째 for문 안에서 100번 도는 중에, 몇 번 잘못 예측했는가 cnt. 
             self.errors_.append(errors)
         print(self.errors_)
         return self
 
     def net_input(self, X):
         """최종 입력 계산"""
-        return np.dot(X, self.w_[1:]) + self.w_[0]
-
+        return np.dot(X, self.w_[1:]) + self.w_[0] # np.dot(A, B) → A와 B 행렬 내적 (==행렬 곱)
+        # z = wTㆍx에서 w : w_[1:], x : X
+        # self.w_[0] : 편차. 상수값. 해당 그래프를 얼마만큼 shift시키는지. ax+b에서 b, ax^2+bx+c에서 c. 보통 이것도 같이 설정해준다. 
+        # x1*w1 + x2*w2 + w0 (w0 : x와는 무관. Deep learning에는 나중에 bias(얼마나 원점에서 shift되는가)라는 개념이 나오는데, 그것과 비슷한 개념)
+        
     def predict(self, X):
         """단위 계단 함수를 사용하여 클래스 레이블을 반환합니다"""
-        return np.where(self.net_input(X) >= 0.0, 1, -1)
+        return np.where(self.net_input(X) >= 0.0, 1, -1) # z값 (self.net_input(X)) 구해서 단위계단함수 ∮(z) = z≥0일때 1이고 아니면 -1인 함수 만듦. 리턴되는 값은 예측값 ∮(z)
 
 
 
@@ -103,7 +108,7 @@ plt.legend(loc='upper left')
 
 plt.show() #그리기~
 
-################################이제 우리가 정의한 class 사용################################
+###### 이제 우리가 정의한 class 사용
 ppn = Perceptron(eta=0.1, n_iter=10)
 
 ppn.fit(X, y)
