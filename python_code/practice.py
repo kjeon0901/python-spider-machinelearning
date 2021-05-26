68b2d7aa95a5a0e48adff679464b71bb24c4b1f8
kjeon0901 <kjeon0901@naver.com>
2021-05-26 10:55:50 +0900

    mod perceptron practice code

--- python_code/practice.py
+++ python_code/practice.py
@@ -136,6 +136,11 @@ def plot_decision_regions(X, y, classifier, resolution=0.02):
     x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
     xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
                            np.arange(x2_min, x2_max, resolution))
+    '''
+    meshgrid(x, y)의 결과
+    xx : x 한 row로 두고 그걸 y 크기만큼 아래로 쫙 복사 -> shape : (y크기, x크기)
+    yy : y 한 column으로 두고 그걸 x 크기만큼 오른쪽으로 쫙 복사 -> shape : (y크기, x크기)
+    '''
     Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
     Z = Z.reshape(xx1.shape)
     plt.contourf(xx1, xx2, Z, alpha=0.3, cmap=cmap)
