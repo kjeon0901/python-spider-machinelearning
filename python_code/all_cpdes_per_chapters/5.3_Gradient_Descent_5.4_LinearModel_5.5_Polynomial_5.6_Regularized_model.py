5ab2b6376f517e6f1504bea6879b90bbf941b1f3
kjeon0901 <kjeon0901@naver.com>
2021-05-27 11:52:16 +0900

    add get_weight_updates function add gradient_descent_steps function which are more to Adaline get predict plot (after final update of weights)

--- python_code/all_cpdes_per_chapters/5.3_Gradient_Descent_5.4_LinearModel_5.5_Polynomial_5.6_Regularized_model.py
+++ python_code/all_cpdes_per_chapters/5.3_Gradient_Descent_5.4_LinearModel_5.5_Polynomial_5.6_Regularized_model.py
@@ -25,11 +25,10 @@ random.rand(m, n) : 0 ~ 1 의 '균일분포 표준정규분포' 랜덤 숫자를
 random.randn(m, n) : 평균 0, 표준편차 1의 '가우시안 표준정규분포' 랜덤 숫자를 (m, n) 크기로 생성
         정규분포 : 0.3 ~ 0.4 사이의 값이 뽑힐 확률과 0.4 ~ 0.5 사이의 값이 뽑힐 확률이 다르다. ex_히스토그램 distplot(정규분포, kde=True)
 '''
-test = np.random.rand(100000,1)
-test2 = np.random.randn(1000,1)
-
-sns.distplot(test, kde=False)  #균일분포
-sns.distplot(test2, kde=False) #정규분포
+#test = np.random.rand(100000,1)
+#test2 = np.random.randn(1000,1)
+#sns.distplot(test, kde=False)  #균일분포
+#sns.distplot(test2, kde=False) #정규분포
 
 # X, y 데이터 셋 scatter plot으로 시각화
 plt.scatter(X, y)
@@ -55,17 +54,17 @@ X.shape, y.shape
 def get_weight_updates(w1, w0, X, y, learning_rate=0.01):
     N = len(y)
     # 먼저 w1_update, w0_update를 각각 w1, w0의 shape와 동일한 크기를 가진 0 값으로 초기화
-    w1_update = np.zeros_like(w1)
+    w1_update = np.zeros_like(w1) # np.zeros_like(변수) : 그 변수와 같은 size를 0으로 채워서 초기화하고 리턴.  
     w0_update = np.zeros_like(w0)
     # 예측 배열 계산하고 예측과 실제 값의 차이 계산
-    y_pred = np.dot(X, w1.T) + w0
+    y_pred = np.dot(X, w1.T) + w0 # [x1*w1 + w0, x2*w2 + w0, ... , x100*w100 + w0] → (100, 1) shape
     diff = y-y_pred
          
     # w0_update를 dot 행렬 연산으로 구하기 위해 모두 1값을 가진 행렬 생성 
     w0_factors = np.ones((N,1))
 
-    # w1과 w0을 업데이트할 w1_update와 w0_update 계산
-    w1_update = -(2/N)*learning_rate*(np.dot(X.T, diff))
+    # w1과 w0을 업데이트할 w1_update와 w0_update 계산  // Δwj = -ηΔJ(w) = -ηJ'(w)
+    w1_update = -(2/N)*learning_rate*(np.dot(X.T, diff)) # ΔJ(w) = dJ/dwj = J'(w) = ㅡΣ( y(i) - y^(i) ) * xj(i) = ㅡ(Y-Y^)ㆍX  ==  np.dot(X.T, diff)
     w0_update = -(2/N)*learning_rate*(np.dot(w0_factors.T, diff))    
     
     return w1_update, w0_update
@@ -99,11 +98,13 @@ def gradient_descent_steps(X, y, iters=10000):
     
     # 인자로 주어진 iters 만큼 반복적으로 get_weight_updates() 호출하여 w1, w0 업데이트 수행. 
     for ind in range(iters):
-        w1_update, w0_update = get_weight_updates(w1, w0, X, y, learning_rate=0.01)
-        w1 = w1 - w1_update
-        w0 = w0 - w0_update
-              
-    return w1, w0
+        w1_update, w0_update = get_weight_updates(w1, w0, X, y, learning_rate=0.01) # Δw1, Δw2 받아옴
+        w1 = w1 - w1_update # w1 = w1 - Δw1
+        w0 = w0 - w0_update # w0 = w0 - Δw0
+        '''
+        모든 샘플을 개별적으로 업데이트하는 게 아니라 iters(epoch)마다 한번에. 퍼셉트론 보다는 '아달린'에 가까움!!
+        '''
+    return w1, w0 # 최종 가중치 리턴 
 
 
 # ** 예측 오차 비용을 계산을 수행하는 함수 생성 및 경사 하강법 수행 **
@@ -116,9 +117,9 @@ def get_cost(y, y_pred):
     cost = np.sum(np.square(y - y_pred))/N
     return cost
 
-w1, w0 = gradient_descent_steps(X, y, iters=1000)
+w1, w0 = gradient_descent_steps(X, y, iters=1000) #최종 업데이트 끝난 가중치 받아옴. 
 print("w1:{0:.3f} w0:{1:.3f}".format(w1[0,0], w0[0,0]))
-y_pred = w1[0,0] * X + w0
+y_pred = w1[0,0] * X + w0  #회귀 계수 : 가중치. x축-X, y축-y_pred인 하나의 1차함수
 print('Gradient Descent Total Cost:{0:.4f}'.format(get_cost(y, y_pred)))
 
 
@@ -126,7 +127,7 @@ print('Gradient Descent Total Cost:{0:.4f}'.format(get_cost(y, y_pred)))
 
 
 plt.scatter(X, y)
-plt.plot(X,y_pred)
+plt.plot(X,y_pred) # X의 feature값이 2개였다면 (column이 2개) -> 3차원의 직선 그래프. 기울기는 얘도 역시 편미분이므로 2개. 
 
 
 # ** 미니 배치 확률적 경사 하강법을 이용한 최적 비용함수 도출 **
