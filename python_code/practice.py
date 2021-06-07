8240aeed205f7293df18e346b2694aec30c61876
kjeon0901 <kjeon0901@naver.com>
2021-06-07 12:44:30 +0900

    add - creditcard prac

--- python_code/practice.py
+++ python_code/practice.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, precision_score, recall_score, roc_auc_score
@@ -13,74 +14,42 @@ from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.ensemble import RandomForestClassifier
 
 
-diabetes_data = pd.read_csv('C:/jeon/diabetes.csv')
+creditcard_data = pd.read_csv('C:/jeon/creditcard.csv')
 
-print(diabetes_data.info()) # null값 x
+print(creditcard_data.info()) # null값 x
 
 '''
-Outcome 1-당뇨병o, 0-당뇨병x
+Outcome : Class 0-사기X 1-사기O (신용카드)
 
-1. 로지스틱 회귀, LGB, XGB, GBM, 랜덤포레스트 이용
-    => 각 estimator 마다 - 정확도, 정밀도, 재현율, f1, roc_auc(predict_proba() 필요함)값 측정
-
-2. Glucose, BloodPressure, Insulin의 0 -> 평균값으로 바꿈
-SkinTickness의 0 ->  임신횟수(Pregnancies) > 0이면 SkinTickness = 25
-                                        == 0                 = 15
-BMI의 0 ->   혈압(BloodPressure) >= 110 이면 BMI의 quentile 75%값 (describe로 확인)
-                            90<= x <110                     50%
-                                 < 90                       25%
+1. 원본 데이터 프레임 복사 후, 로지스틱회귀와 LGB 이용해서 정확도, 정밀도, 재현율, F1, roc_auc 구하기
+2. corr()함수로 데이터 간의 상관관계 계수 구한 후, heatmap 그리기
+3. 레이블 데이터와 상관계수의 '절댓값'이 가장 높은 3개의 feature데이터가 뭔지 구하기
 
 '''
 
-
-X = diabetes_data.iloc[:, :-1]
-y = diabetes_data.iloc[:, -1]
-X_describe = X.describe()
-
-zero_to_mean=['Glucose', 'BloodPressure', 'Insulin']
-for each in zero_to_mean:
-    mean = int(X[X[each]!=0][each].agg('mean'))
-    X[each] = X[each].apply(lambda x:mean if x==0 else x)
-
-test = X[X['SkinThickness']==0]['Pregnancies'].apply(lambda x:x if x!=0 else(25 if x>0 else 15))
-indexes = test.index
-X['SkinThickness'][indexes] = test[indexes]
-
-test = X[X['BMI']==0]['BloodPressure'].apply(lambda x:X_describe['BMI']['75%'] if x >= 110 \
-                                    else(X_describe['BMI']['50%'] if (x>=90 and x<110) else X_describe['BMI']['25%']))
-indexes = test.index
-X['BMI'][indexes] = test[indexes]
-
+X = creditcard_data.iloc[:, :-1]
+y = creditcard_data.iloc[:, -1]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=156, stratify=y)
 
-#아래에서 인수로 추가해준 건 warning 제거해주기 위해 추가함. 안써줘도 상관없음. 
 lr_clf = LogisticRegression(max_iter=200)
 lgb_clf = LGBMClassifier()
-xgb_clf = XGBClassifier(use_label_encoder=False, eval_metric='mlogloss')
-gbm_clf = GradientBoostingClassifier()
-rf_clf = RandomForestClassifier()
 
-models = [lr_clf, lgb_clf, xgb_clf, gbm_clf, rf_clf]
+models = [lr_clf, lgb_clf]
 for idx, model in enumerate(models):
     model.fit(X_train, y_train)
     pred = model.predict(X_test)
     pred_proba = model.predict_proba(X_test)[:, 1]
-    
-    print("\n",end='')
-    print(idx+1)
-    print("정확도 : {0:.4f}".format(accuracy_score(pred, y_test)))
+
+    print("\n정확도 : {0:.4f}".format(accuracy_score(pred, y_test)))
     print("정밀도 : {0:.4f}".format(precision_score(pred, y_test)))
     print("재현율 : {0:.4f}".format(recall_score(pred, y_test)))
     print("f1 : {0:.4f}".format(f1_score(pred, y_test)))
     print("roc_auc : {0:.4f}".format(roc_auc_score(y_test, pred_proba)))
     
+corr = creditcard_data.corr()
+sns.heatmap(corr, cmap = 'RdBu')
 
-    
-    
-    
-    
-    
-    
-    
-    
+label_corr = corr.iloc[:-1, -1] # 레이블 데이터와의 상관계수
+label_corr = abs(label_corr).sort_values(ascending=False)
+print(label_corr.head(3))
